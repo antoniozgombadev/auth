ce5af2f563201ce0e32ff414c34edd3bf262aa35
Antonio Zgomba <antoniozgomba.dev@gmail.com>
2026-03-03 10:13:27 +0100

    Prepare for Render deploy

--- app.py
+++ app.py
@@ -73,4 +73,8 @@ def profile():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
