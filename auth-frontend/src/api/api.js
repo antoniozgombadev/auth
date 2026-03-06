32bf63d89a6ab00db17820fca2f0dd382f345710
Antonio Zgomba <antoniozgomba.dev@gmail.com>
2026-03-06 09:36:44 +0100

    fix requirements path

--- auth-frontend/src/api/api.js
+++ auth-frontend/src/api/api.js
@@ -1,25 +1,17 @@
-const API_URL = "https://flask-auth-api-cnjc.onrender.com/";
+const API_URL = "https://flask-auth-api-cnjc.onrender.com";
 
 export const apiFetch = async (url, options = {}) => {
   const token = localStorage.getItem("token");
 
   const res = await fetch(`${API_URL}${url}`, {
-    ...options,
+    method: options.method || "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-      ...(options.headers || {}),
+      Authorization: token ? `Bearer ${token}` : "",
+      ...(options.headers || {})
     },
+    ...options
   });
 
   return res.json();
 };
-const res = await fetch(`${API_URL}${url}`, {
-  method: options.method || "GET",
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: token ? `Bearer ${token}` : "",
-    ...(options.headers || {})
-  },
-  ...options
-});
